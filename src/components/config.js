3abebef176befe747d97951862a47ac3aff77fbd
Harikaran1212 <131746803+Harikaran1212@users.noreply.github.com>
2024-12-01 19:39:46 +0530

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://phaserbouncinggame-backend.onrender.com';
 
 export { BACKEND_URL };
